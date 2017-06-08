a12a34e6611f5e6e23287053af642f9912c9c17e
rabinovichm <michu.rabinovich@hotmail.com>
2017-06-08 04:11:16 -0300

    agrego drop and create para la procedure de altaUsuario

--- src/UberFrba/Registro usuario/RegistroUsuario.cs
+++ src/UberFrba/Registro usuario/RegistroUsuario.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Connection;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Utilities;
 
 namespace UberFrba.Registro_usuario
 {
@@ -47,30 +50,91 @@ namespace UberFrba.Registro_usuario
         private void btnRegistrar_Click(object sender, EventArgs e)
         {
 
-
-            //tengo que verificar que sean no nulos antes de parsear
-            string nombre = txtNombre.Text;
-            string apellido = txtApellido.Text;
-            int dni = int.Parse(txtDni.Text);
-            
-            string contra = txtContrasenia.Text;
-            string confContra = txtConfContrasenia.Text;
-            int diaNac = int.Parse(txtDiaNac.Text);
-            int mesNac = int.Parse(txtMesNac.Text);
-            int anioNac = int.Parse(txtAnioNac.Text);
-            int tel = int.Parse(txtTel.Text);
-            string mail = txtMail.Text;
-            string calle = txtCalle.Text;
-            string depto = txtDepto.Text;
-            int piso = int.Parse(txtPiso.Text);
-
+            string nombre;
+            string apellido;
+            int dni;
+            string contra;
+            string confContra;
+            int diaNac;
+            int mesNac;
+            int anioNac;
+            int tel;
+            string mail;
+            string calle;
+            string depto;
+            int piso;
+            string localidad;
+            int esChofer = 0;
+            int esCliente = 0;
+            DateTime fechaNac;
 
             if(txtNombre.Text=="" || txtApellido.Text =="" || txtDni.Text=="" || txtContrasenia.Text=="" || 
                 txtConfContrasenia.Text =="" || txtDiaNac.Text=="" || txtMesNac.Text=="" || txtAnioNac.Text=="" || 
-                txtTel.Text=="" || txtMail.Text=="" || txtCalle.Text=="" || txtDepto.Text=="" || txtPiso.Text==""){
+                txtTel.Text=="" || txtMail.Text=="" || txtCalle.Text=="" || txtDepto.Text=="" || txtPiso.Text=="")
+            {
+                MessageBox.Show("Complete todos los campos por favor");
+            }
+            else if (!chkCliente.Checked && !chkChofer.Checked)
+            {
+                MessageBox.Show("Indique el tipo de usuario");
+            }
+            else if(txtContrasenia.Text != txtContrasenia.Text)
+            {
+                MessageBox.Show("Las constrasenias no coinciden");
+            }//podria validar qeu los int sean numericos posta
+            else 
+            {
+                nombre = txtNombre.Text;
+                apellido = txtApellido.Text;
+                dni = int.Parse(txtDni.Text);
+                contra = txtContrasenia.Text;
+                confContra = txtConfContrasenia.Text;
+                diaNac = int.Parse(txtDiaNac.Text);
+                mesNac = int.Parse(txtMesNac.Text);
+                anioNac = int.Parse(txtAnioNac.Text);
+                fechaNac = new DateTime(anioNac, mesNac, diaNac);
+                tel = int.Parse(txtTel.Text);
+                mail = txtMail.Text;
+                calle = txtCalle.Text;
+                depto = txtDepto.Text;
+                piso = int.Parse(txtPiso.Text);
+                localidad = txtLocalidad.Text;
+                
+                if (chkChofer.Checked) {
+                    esChofer = 1;
+                }
+                if (chkCliente.Checked)
+                {
+                    esCliente = 1;
+                }
 
-                    MessageBox.Show("Complete todos los campos por favor");
-            } //else if(!chkCliente.Checked() && chkChofer.
+               //falta encriptar la contrasenia
+
+
+                List<SqlParameter> parameterList = new List<SqlParameter>();
+                parameterList.Add(new SqlParameter("@nombre", nombre));
+                parameterList.Add(new SqlParameter("@apellido", apellido));
+                parameterList.Add(new SqlParameter("@direccion", calle));
+                parameterList.Add(new SqlParameter("@telefono", tel));
+                parameterList.Add(new SqlParameter("@dni", dni));
+                parameterList.Add(new SqlParameter("@fechaNac", fechaNac));
+                parameterList.Add(new SqlParameter("@contrasenia", contra)); 
+                parameterList.Add(new SqlParameter("@mail", mail));
+                parameterList.Add(new SqlParameter("@piso", piso));
+                parameterList.Add(new SqlParameter("@depto", depto));
+                parameterList.Add(new SqlParameter("@localidad", localidad));
+                parameterList.Add(new SqlParameter("@esChofer", esChofer));
+                parameterList.Add(new SqlParameter("@esCliente", esCliente));
+
+
+//    public static int ExecuteNonQuery(string nombreProcedure, CommandType Tipo, List<SqlParameter> ParameterList)
+                
+                string nomProcedure ="PR_altaUsuario";
+                int result = SQLHelper.ExecuteNonQuery(nomProcedure, CommandType.StoredProcedure, parameterList);
+
+                MessageBox.Show("cant filas afectadas" + result);
+
+            }
 
         }
     }
