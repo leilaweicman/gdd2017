7f6b01305ed13fb3599cc1952e632572ff968ddd
Leila Weicman <leilaweic@hotmail.com>
2017-06-12 09:45:06 -0300

    agrego procedure para buscar roles por usuario

--- data/queriesLei.sql
+++ data/queriesLei.sql
@@ -1,18 +1,32 @@
 
-IF OBJECT_ID ('GIRLPOWER.PR_altaUsuario', 'P') IS NOT NULL
+IF OBJECT_ID ('GIRLPOWER.PR_traerUsuarioPorUsername', 'P') IS NOT NULL
 DROP PROCEDURE [GIRLPOWER].[PR_traerUsuarioPorUsername]
 GO
 
-IF OBJECT_ID ('GIRLPOWER.PR_altaUsuario', 'P') IS NOT NULL
+IF OBJECT_ID ('GIRLPOWER.deshabilitarUsuario', 'P') IS NOT NULL
 DROP PROCEDURE [GIRLPOWER].[deshabilitarUsuario]
+GO
+
+IF OBJECT_ID ('GIRLPOWER.traerListadoRolPorUsuario', 'P') IS NOT NULL
+DROP PROCEDURE [GIRLPOWER].[traerListadoRolPorUsuario]
 GO
 
 CREATE PROCEDURE [GIRLPOWER].[PR_traerUsuarioPorUsername] (@Username VARCHAR(30)) AS
 BEGIN
 	SELECT * FROM girlpower.usuario WHERE username=@Username
 END
+GO
 
 CREATE PROCEDURE [GIRLPOWER].[deshabilitarUsuario] (@IdUsuario VARCHAR(30)) AS
 BEGIN
 	UPDATE girlpower.usuario SET Habilitado=0 WHERE IDUsuario=@IdUsuario
 END
+GO
+
+CREATE PROCEDURE [GIRLPOWER].[traerListadoRolPorUsuario] (@IDUsuario int) AS
+BEGIN
+	SELECT * FROM girlpower.RolPorUsuario r1
+	join girlpower.Rol r2 on r1.IDRol = r2.IDRol
+	WHERE r1.IDUsuario=@IDUsuario
+END
+GO
