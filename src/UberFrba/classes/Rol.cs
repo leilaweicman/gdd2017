3d24e54d1c8294cf28b04280a30ee4bbf7de83a6
Leila Weicman <leilaweic@hotmail.com>
2017-06-12 09:46:10 -0300

    creo clase Rol

---
+++ src/UberFrba/classes/Rol.cs
@@ -0,0 +1,99 @@
+﻿using Connection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Rol : Base
+    {
+        List<SqlParameter> parameterList = new List<SqlParameter>();
+
+        #region atributos
+        private int _id_Rol;
+        private string _nombre;
+        private bool _habilitado;
+        //List<Funcionalidad> _funcionalidades = new List<Funcionalidad>();
+        #endregion
+
+        #region properties
+        public int Id_Rol
+        {
+            get { return _id_Rol; }
+            set { _id_Rol = value; }
+        }
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public bool Habilitado
+        {
+            get { return _habilitado; }
+            set { _habilitado = value; }
+        }
+        
+        //public List<Funcionalidad> Funcionalidades
+        //{
+        //    get { return _funcionalidades; }
+        //    set { _funcionalidades = value; }
+        //}
+        #endregion
+
+        #region constructor
+        public Rol()
+        {
+            this.Id_Rol = 0;
+            this.Nombre = "";
+            this.Habilitado = false;
+
+        }
+        
+        #endregion
+
+        #region metodos publicos
+        public override string NombreTabla()
+        {
+            return "Rol";
+        }
+
+        public override string NombreEntidad()
+        {
+            return "Rol";
+        }
+
+        public override void DataRowToObject(DataRow dr)
+        {
+            // Esto es tal cual lo devuelve el stored de la DB
+            this.Id_Rol = Convert.ToInt32(dr["IDRol"]);
+            this.Nombre = dr["Nombre"].ToString();
+            this.Habilitado = Convert.ToBoolean(dr["Habilitado"]);
+        }
+
+        public static DataSet ObtenerRolesPorUsuario(int id_Usuario)
+        {
+            Rol rol = new Rol();
+            rol.setearListaDeParametrosConIdUsuario(id_Usuario);
+            DataSet ds = rol.TraerListado(rol.parameterList, "PorUsuario");
+            rol.parameterList.Clear();
+
+            return ds;
+        }
+        
+        #endregion
+
+        #region metodos privados
+        private void setearListaDeParametrosConIdUsuario(int id_Usuario)
+        {
+            parameterList.Add(new SqlParameter("@IDUsuario", id_Usuario));
+        }
+
+        #endregion
+
+    }
+}
