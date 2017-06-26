8ccc905325c1c5da49e539169137ffecd08ed444
Leila Weicman <leilaweic@hotmail.com>
2017-06-25 21:05:31 -0300

    agrego sp para traer las funcionalidades de un rol

--- data/queriesLei.sql
+++ data/queriesLei.sql
@@ -11,6 +11,10 @@ IF OBJECT_ID ('GIRLPOWER.traerListadoRolPorUsuario', 'P') IS NOT NULL
 DROP PROCEDURE [GIRLPOWER].[traerListadoRolPorUsuario]
 GO
 
+IF OBJECT_ID ('GIRLPOWER.traerListadoFuncionalidadPorRol', 'P') IS NOT NULL
+DROP PROCEDURE [GIRLPOWER].[traerListadoFuncionalidadPorRol]
+GO
+
 CREATE PROCEDURE [GIRLPOWER].[PR_traerUsuarioPorUsername] (@Username VARCHAR(30)) AS
 BEGIN
 	SELECT * FROM girlpower.usuario WHERE username=@Username
@@ -28,5 +32,13 @@ BEGIN
 	SELECT * FROM girlpower.RolPorUsuario r1
 	join girlpower.Rol r2 on r1.IDRol = r2.IDRol
 	WHERE r1.IDUsuario=@IDUsuario
+END
+GO
+
+CREATE PROCEDURE [GIRLPOWER].[traerListadoFuncionalidadPorRol] (@IDRol int) AS
+BEGIN
+	SELECT * FROM girlpower.FuncionalidadPorRol f
+	join girlpower.Funcionalidad f2 on f.IDFuncionalidad = f2.IDFuncionalidad
+	WHERE f.IDRol=@IDRol
 END
 GO
