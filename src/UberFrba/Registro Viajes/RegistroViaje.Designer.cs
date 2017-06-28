44c616f6f420f8b1eb42be53262af0d55f93c875
Leila Weicman <leilaweic@hotmail.com>
2017-06-28 18:38:10 -0300

    empiezo con el registro de viajes

---
+++ src/UberFrba/Registro Viajes/RegistroViaje.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace UberFrba.Registro_Viajes
+{
+    partial class RegistroViaje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpTurnos = new System.Windows.Forms.GroupBox();
+            this.lblChofer = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtAutomovil = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lbAutomovil = new System.Windows.Forms.Label();
+            this.lblTurno = new System.Windows.Forms.Label();
+            this.cmbTurno = new System.Windows.Forms.ComboBox();
+            this.txtKilometros = new System.Windows.Forms.TextBox();
+            this.lblKilometros = new System.Windows.Forms.Label();
+            this.txtFechaInicio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFechaFin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.cmbCliente = new System.Windows.Forms.ComboBox();
+            this.grpTurnos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpTurnos
+            // 
+            this.grpTurnos.Controls.Add(this.lblCliente);
+            this.grpTurnos.Controls.Add(this.cmbCliente);
+            this.grpTurnos.Controls.Add(this.txtFechaFin);
+            this.grpTurnos.Controls.Add(this.label2);
+            this.grpTurnos.Controls.Add(this.txtFechaInicio);
+            this.grpTurnos.Controls.Add(this.label1);
+            this.grpTurnos.Controls.Add(this.txtKilometros);
+            this.grpTurnos.Controls.Add(this.lblKilometros);
+            this.grpTurnos.Controls.Add(this.cmbTurno);
+            this.grpTurnos.Controls.Add(this.lblTurno);
+            this.grpTurnos.Controls.Add(this.lblChofer);
+            this.grpTurnos.Controls.Add(this.comboBox1);
+            this.grpTurnos.Controls.Add(this.btnRegistrar);
+            this.grpTurnos.Controls.Add(this.txtAutomovil);
+            this.grpTurnos.Controls.Add(this.btnCancelar);
+            this.grpTurnos.Controls.Add(this.lbAutomovil);
+            this.grpTurnos.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpTurnos.Location = new System.Drawing.Point(43, 12);
+            this.grpTurnos.Name = "grpTurnos";
+            this.grpTurnos.Size = new System.Drawing.Size(596, 576);
+            this.grpTurnos.TabIndex = 2;
+            this.grpTurnos.TabStop = false;
+            this.grpTurnos.Text = "Turnos";
+            // 
+            // lblChofer
+            // 
+            this.lblChofer.AutoSize = true;
+            this.lblChofer.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChofer.Location = new System.Drawing.Point(16, 34);
+            this.lblChofer.Name = "lblChofer";
+            this.lblChofer.Size = new System.Drawing.Size(58, 18);
+            this.lblChofer.TabIndex = 48;
+            this.lblChofer.Text = "Chofer";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(19, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 26);
+            this.comboBox1.TabIndex = 47;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(19, 524);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(103, 35);
+            this.btnRegistrar.TabIndex = 46;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // txtAutomovil
+            // 
+            this.txtAutomovil.Location = new System.Drawing.Point(19, 116);
+            this.txtAutomovil.Name = "txtAutomovil";
+            this.txtAutomovil.Size = new System.Drawing.Size(173, 27);
+            this.txtAutomovil.TabIndex = 12;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(148, 524);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(103, 35);
+            this.btnCancelar.TabIndex = 45;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // lbAutomovil
+            // 
+            this.lbAutomovil.AutoSize = true;
+            this.lbAutomovil.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAutomovil.Location = new System.Drawing.Point(16, 95);
+            this.lbAutomovil.Name = "lbAutomovil";
+            this.lbAutomovil.Size = new System.Drawing.Size(83, 18);
+            this.lbAutomovil.TabIndex = 7;
+            this.lbAutomovil.Text = "Automovil";
+            // 
+            // lblTurno
+            // 
+            this.lblTurno.AutoSize = true;
+            this.lblTurno.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurno.Location = new System.Drawing.Point(16, 161);
+            this.lblTurno.Name = "lblTurno";
+            this.lblTurno.Size = new System.Drawing.Size(51, 18);
+            this.lblTurno.TabIndex = 49;
+            this.lblTurno.Text = "Turno";
+            // 
+            // cmbTurno
+            // 
+            this.cmbTurno.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTurno.FormattingEnabled = true;
+            this.cmbTurno.Location = new System.Drawing.Point(19, 182);
+            this.cmbTurno.Name = "cmbTurno";
+            this.cmbTurno.Size = new System.Drawing.Size(173, 26);
+            this.cmbTurno.TabIndex = 50;
+            // 
+            // txtKilometros
+            // 
+            this.txtKilometros.Location = new System.Drawing.Point(19, 251);
+            this.txtKilometros.Name = "txtKilometros";
+            this.txtKilometros.Size = new System.Drawing.Size(173, 27);
+            this.txtKilometros.TabIndex = 52;
+            // 
+            // lblKilometros
+            // 
+            this.lblKilometros.AutoSize = true;
+            this.lblKilometros.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKilometros.Location = new System.Drawing.Point(16, 230);
+            this.lblKilometros.Name = "lblKilometros";
+            this.lblKilometros.Size = new System.Drawing.Size(88, 18);
+            this.lblKilometros.TabIndex = 51;
+            this.lblKilometros.Text = "Kilómetros";
+            // 
+            // txtFechaInicio
+            // 
+            this.txtFechaInicio.Location = new System.Drawing.Point(19, 322);
+            this.txtFechaInicio.Name = "txtFechaInicio";
+            this.txtFechaInicio.Size = new System.Drawing.Size(173, 27);
+            this.txtFechaInicio.TabIndex = 54;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 18);
+            this.label1.TabIndex = 53;
+            this.label1.Text = "Fecha Inicio";
+            // 
+            // txtFechaFin
+            // 
+            this.txtFechaFin.Location = new System.Drawing.Point(19, 392);
+            this.txtFechaFin.Name = "txtFechaFin";
+            this.txtFechaFin.Size = new System.Drawing.Size(173, 27);
+            this.txtFechaFin.TabIndex = 56;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 371);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 18);
+            this.label2.TabIndex = 55;
+            this.label2.Text = "FechaFin";
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(16, 443);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(58, 18);
+            this.lblCliente.TabIndex = 58;
+            this.lblCliente.Text = "Cliente";
+            // 
+            // cmbCliente
+            // 
+            this.cmbCliente.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCliente.FormattingEnabled = true;
+            this.cmbCliente.Location = new System.Drawing.Point(19, 464);
+            this.cmbCliente.Name = "cmbCliente";
+            this.cmbCliente.Size = new System.Drawing.Size(173, 26);
+            this.cmbCliente.TabIndex = 57;
+            // 
+            // RegistroViaje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 609);
+            this.Controls.Add(this.grpTurnos);
+            this.Name = "RegistroViaje";
+            this.Text = "RegistroViaje";
+            this.grpTurnos.ResumeLayout(false);
+            this.grpTurnos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpTurnos;
+        private System.Windows.Forms.Label lblChofer;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtAutomovil;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lbAutomovil;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.ComboBox cmbCliente;
+        private System.Windows.Forms.TextBox txtFechaFin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFechaInicio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKilometros;
+        private System.Windows.Forms.Label lblKilometros;
+        private System.Windows.Forms.ComboBox cmbTurno;
+        private System.Windows.Forms.Label lblTurno;
+    }
+}
