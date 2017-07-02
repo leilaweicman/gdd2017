3f1daecd370c0160d085bef4d9478f5885dbe97a
Leila Weicman <leilaweic@hotmail.com>
2017-07-02 02:22:32 -0300

    modifico el nombre de las queries de estadisticas

--- data/queriesEstadisticas.sql
+++ data/queriesEstadisticas.sql
@@ -1,8 +1,9 @@
-IF OBJECT_ID ('GIRLPOWER.PR_ChoferesConMasRecaudacion', 'P') IS NOT NULL
-DROP PROCEDURE [GIRLPOWER].[PR_ChoferesConMasRecaudacion]
+
+IF OBJECT_ID ('GIRLPOWER.traerListadoUsuarioChoferesConMasRecaudacion', 'P') IS NOT NULL
+DROP PROCEDURE [GIRLPOWER].[traerListadoUsuarioChoferesConMasRecaudacion]
 GO
 
-create procedure [girlpower].PR_ChoferesConMasRecaudacion(@fechaInicio date, @fechaFin date)
+create procedure [girlpower].[traerListadoUsuarioChoferesConMasRecaudacion] (@fechaInicio date, @fechaFin date)
 as begin
 ---CHOFERES CON MAS RECAUDACION
 select top 5 u.Nombre, sum(r.ImporteTotal) as Total
@@ -15,12 +16,28 @@ order by 2 desc
 
 end
 go 
-IF OBJECT_ID ('GIRLPOWER.PR_ChoferesConViajeMasLargo', 'P') IS NOT NULL
-DROP PROCEDURE [GIRLPOWER].[PR_ChoferesConViajeMasLargo]
+IF OBJECT_ID ('GIRLPOWER.traerListadoUsuarioChoferesConViajeMasLargo', 'P') IS NOT NULL
+DROP PROCEDURE [GIRLPOWER].[traerListadoUsuarioChoferesConViajeMasLargo]
 
 GO
 
-create procedure [girlpower].PR_ChoferesConViajeMasLargo(@fechaInicio date, @fechaFin date)
+create procedure [girlpower].[traerListadoUsuarioChoferesConViajeMasLargo] (@fechaInicio date, @fechaFin date)
+as begin
+select top 5 u.Nombre, max(v.CantidadKilometros)as Kilometros
+from [GIRLPOWER].Viaje v join [GIRLPOWER].Cliente c
+on v.IDCliente=c.IDCliente
+join [GIRLPOWER].Usuario u on u.IDUsuario=c.IDUsuario
+where v.FechaInicio between @fechaInicio and @fechaFin
+group by v.IDCliente,u.Nombre
+order by 2 desc
+end
+go
+
+IF OBJECT_ID ('GIRLPOWER.traerListadoUsuarioClientesConMayorConsumo', 'P') IS NOT NULL
+DROP PROCEDURE [GIRLPOWER].[traerListadoUsuarioClientesConMayorConsumo]
+GO
+
+create procedure [girlpower].[traerListadoUsuarioClientesConMayorConsumo](@fechaInicio date, @fechaFin date)
 as begin
 select top 5 u.Nombre, sum(f.ImporteTotal) as Total
 from [GIRLPOWER].Factura f join [GIRLPOWER].Cliente c
@@ -31,27 +48,13 @@ group by f.IDCliente,u.Nombre
 order by 2 desc
 end
 go
-IF OBJECT_ID ('GIRLPOWER.PR_ClientesConMayorConsumo', 'P') IS NOT NULL
-DROP PROCEDURE [GIRLPOWER].[PR_ClientesConMayorConsumo]
-GO
 
-create procedure [girlpower].PR_ClientesConMayorConsumo(@fechaInicio date, @fechaFin date)
-as begin
-select top 5 u.Nombre, sum(f.ImporteTotal) as Total
-from [GIRLPOWER].Factura f join [GIRLPOWER].Cliente c
-on f.IDCliente=c.IDCliente
-join [GIRLPOWER].Usuario u on u.IDUsuario=c.IDUsuario
-where f.Fecha between @fechaInicio and @fechaFin
-group by f.IDCliente,u.Nombre
-order by 2 desc
-end
-go
-IF OBJECT_ID ('GIRLPOWER.PR_ClienteQueUtilizoMasVecesElMismoAuto', 'P') IS NOT NULL
-DROP PROCEDURE [GIRLPOWER].[PR_ClienteQueUtilizoMasVecesElMismoAuto]
+IF OBJECT_ID ('GIRLPOWER.traerListadoUsuarioClienteQueUtilizoMasVecesElMismoAuto', 'P') IS NOT NULL
+DROP PROCEDURE [GIRLPOWER].[traerListadoUsuarioClienteQueUtilizoMasVecesElMismoAuto]
 
 GO
 
-create procedure [girlpower].PR_ClienteQueUtilizoMasVecesElMismoAuto(@fechaInicio date, @fechaFin date)
+create procedure [girlpower].[traerListadoUsuarioClienteQueUtilizoMasVecesElMismoAuto] (@fechaInicio date, @fechaFin date)
 as begin
 select top 5 u.Nombre, a.Patente, count(*) as Cantidad from [GIRLPOWER].Viaje v
 join [GIRLPOWER].Cliente c on c.IDCliente=v.IDCliente
